95f9926ee990c325e30ba9fb6ab7e9789d4d5bb9
Yan-ice <2364728692@qq.com>
2021-09-22 19:39:55 +0800

    half

--- main.cpp
+++ main.cpp
@@ -6,23 +6,24 @@ using namespace std;
 
 
 int main() {
-    cout<<(int)'\0'<<endl;
-    ifstream in1 = ifstream();
-//从文件读取信息
-    ofstream out1 = ofstream();
-//向文件写入信息
-    ifstream in2 = ifstream();
-//从文件读取信息
-    ofstream out2 = ofstream();
-//向文件写入信息
-
-    in1.open("..//testfile//sourceFile.txt",ios::out | ios::in);
-    out1.open("..//testfile//compressedFile.txt",ios::out | ios::in);
-    compress(in1,out1,75,75);
-
-    in2.open("..//testfile//compressedFile.txt",ios::out | ios::in);
-    out2.open("..//testfile//uncompressedFile.txt",ios::out | ios::in);
-    decompress(in2,out2,75);
+    for(size_t i = 10; i >= 0; i--) cout << "Line " << i << endl;
+//    cout<<(int)'\0'<<endl;
+//    ifstream in1 = ifstream();
+////从文件读取信息
+//    ofstream out1 = ofstream();
+////向文件写入信息
+//    ifstream in2 = ifstream();
+////从文件读取信息
+//    ofstream out2 = ofstream();
+////向文件写入信息
+//
+//    in1.open("..//testfile//sourceFile.txt",ios::out | ios::in);
+//    out1.open("..//testfile//compressedFile.txt",ios::out | ios::in);
+//    compress(in1,out1,75,75);
+//
+//    in2.open("..//testfile//compressedFile.txt",ios::out | ios::in);
+//    out2.open("..//testfile//uncompressedFile.txt",ios::out | ios::in);
+//    decompress(in2,out2,75);
 //
 //    in1.open("..//testfile//compressedFile.txt",ios::out | ios::in);
 //    out1.open("..//testfile//compressedFile2.txt",ios::out | ios::in);
